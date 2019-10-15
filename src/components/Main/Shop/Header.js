10a03c2c1fe35f3a30967f91146b1718413472a5
SonHoang <sonhoangvinhse61570@gmail.com>
2019-10-15 16:17:30 +0700

    done header

--- src/components/Main/Shop/Header.js
+++ src/components/Main/Shop/Header.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
-import { View, TextInput, Dimensions, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TextInput, Dimensions, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import icMenu from '../../../../images/menu.png';
+import icLogo from '../../../../images/logo.png';
 
 const { height } = Dimensions.get('window');
 
 export default class Header extends Component {
     render() {
         return (
-            <View style={{ height: height / 8, backgroundColor: '#EA7171', flexDirection: 'row' }}>
-                <TouchableOpacity 
-                    onPress={this.props.onOpen} 
-                    style={{ flex: 1, justifyContent: 'center', marginLeft: 5 }}
-                >
-                    <Image 
-                        source={require("../../../../images/menu.png")} 
-                    />
-                </TouchableOpacity>
-                <TextInput style={{ flex: 5, backgroundColor: 'white', justifyContent: 'center', height: 35 }}>
-                    Lazada
-                </TextInput>
+            <View style={styles.wrapper}>
+               <View style={styles.row1}>
+                    <TouchableOpacity onPress={this.props.onOpen} >
+                        <Image source={icMenu} style={styles.iconStyle} />
+                    </TouchableOpacity>
+                    <Text style={styles.titleStyle}>Lazada</Text>
+                    <Image source={icLogo} style={styles.iconStyle} />
+                </View>
+                <TextInput style={styles.textInput} placeholder='Search' />
             </View>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    wrapper: {
+        height: height / 7, 
+        backgroundColor: '#EA7171',
+        padding: 8,
+        justifyContent: 'space-around'
+    },
+    row1: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+    },
+    textInput: {
+        backgroundColor: 'white',
+        height: height / 15
+    },
+    iconStyle: {
+        width: 25,
+        height: 25
+    },
+    titleStyle: {
+        color: 'white',
+        fontSize: 20
+    },
+})
