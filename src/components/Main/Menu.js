aefd365a2179ed9fb2d0bf3b681aaa3735de4ebe
SonHoang <sonhoangvinhse61570@gmail.com>
2019-10-16 16:44:28 +0700

    logout layout

--- src/components/Main/Menu.js
+++ src/components/Main/Menu.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import profileIcon from '../../../images/profile.png';
 
 export default class Menu extends Component {
     goToAuthentication() {
         const { navigator } = this.props;
         navigator.push({ name: "AUTHENTICATION" });
     }
-
     goToChangeInfo() {
         const { navigator } = this.props;
         navigator.push({ name: "CHANGE_INFO" });
     }
-
     goToOrderHistory() {
         const { navigator } = this.props;
         navigator.push({ name: "ORDER_HISTORY" });
     }
-
     render() {
+        const { container, profile, btnStyle, btnText } = styles;
         return (
-            <View style={{ backgroundColor: 'yellow', flex: 1 }}>
-                <TouchableOpacity onPress={() => this.goToAuthentication()} >
-                    <Text>Authentication</Text>
-                </TouchableOpacity>
-                <TouchableOpacity  onPress={() => this.goToChangeInfo()} >
-                    <Text>Change Info</Text>
-                </TouchableOpacity>
-                <TouchableOpacity  onPress={() => this.goToOrderHistory()} >
-                    <Text>Order History</Text>
-                </TouchableOpacity>
+            <View style={container}>
+                <Image source={profileIcon} style={profile} />
+                <View>
+                    <TouchableOpacity style={btnStyle}>
+                        <Text style={btnText}>Sign In</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#63C491',
+        flex: 1,
+        borderRightWidth: 3,
+        borderRightColor: '#ECE3E3',
+        alignItems: 'center',
+    },
+    profile: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        marginBottom: 20,
+    },
+    btnStyle: {
+        height: 60,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        paddingHorizontal: 70,
+    },
+    btnText: {
+        color: '#63C491',
+    },
+})
